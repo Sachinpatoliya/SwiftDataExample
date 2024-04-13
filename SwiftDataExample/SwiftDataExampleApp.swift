7617e94647e39b3e65ccacd42b838cce1481da4d
Sachin Patoliya <sachinpatoliya24@gmail.com>
2024-04-13 11:39:40 +0530

    Initial Commit

---
+++ SwiftDataExample/SwiftDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExampleApp.swift
+//  SwiftDataExample
+//
+//  Created by Sachin Patoliya on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
